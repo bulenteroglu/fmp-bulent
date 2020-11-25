9a99749f9b16df404fd9045a179c71ff3cf69b4a
Bulent <eroglu.bulent98@gmail.com>
2020-11-25 15:39:14 +0000

    table operating currently - user input bug still remains

--- packages/frontend/src/components/Table.jsx
+++ packages/frontend/src/components/Table.jsx
@@ -3,19 +3,47 @@ import React from 'react';
 export default function Table({ cols, rows }) {
   if (!cols) return null;
 
+  function handleTableData(data) {
+    let product = [];
+    let multiply = [];
+
+    for (let i = 0; i < data.length; i++) {
+      if (data[i]) {
+        multiply = data.map((cur) => data[i] * cur);
+      }
+      product.push([...multiply]);
+    }
+    return product;
+  }
+
+  function displayTable(data) {
+    return handleTableData(data);
+  }
+
   return (
     <table>
-      <tr>
-        <th>x</th>
-        {cols.map((col, i) => (
-          <th key={i}>{col}</th>
-        ))}
-      </tr>
-      {rows.map((row, i) => (
-        <tr key={i}>
-          <td>{row}</td>
+      <thead>
+        <tr>
+          <th></th>
+          {cols.length > 0 &&
+            cols.map((data, i) => {
+              return <th key={i}>{data}</th>;
+            })}
         </tr>
-      ))}
+      </thead>
+      <tbody>
+        {cols.length > 0 &&
+          cols.map((col, i) => {
+            return (
+              <tr key={i}>
+                <th key={i}>{col}</th>
+                {displayTable(cols)[i].map((product, i) => {
+                  return <td key={i}>{product}</td>;
+                })}
+              </tr>
+            );
+          })}
+      </tbody>
     </table>
   );
 }
